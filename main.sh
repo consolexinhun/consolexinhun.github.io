8e554a70e196913a7afab29195699764b4efeca6
consolexinhun <consolexin@gmail.com>
2021-10-22 21:56:39 +0800

    没变

--- main.sh
+++ main.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-msg='拓扑排序'
+msg='没变'
+
 githubUrl=git@github.com:consolexinhun/consolexinhun.github.io.git
 
 git pull origin master
@@ -14,12 +15,13 @@ yarn build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-GITHUB_TOKEN="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"
+# GITHUB_TOKEN="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"
 
-githubUrl=https://consolexinhun:${GITHUB_TOKEN}@github.com/consolexinhun/consolexinhun.github.io.git
 
-git config --global user.name "consolexinhun"
-git config --global user.email "consolexin@gmail.com"
+# githubUrl=https://consolexinhun:${GITHUB_TOKEN}@github.com/consolexinhun/consolexinhun.github.io.git
+
+# git config --global user.name "consolexinhun"
+# git config --global user.email "consolexin@gmail.com"
 
 git init
 git add -A
@@ -27,7 +29,8 @@ git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 
-cd -
+# cd -
+cd ../../..
 rm -rf docs/.vuepress/dist
 
 
