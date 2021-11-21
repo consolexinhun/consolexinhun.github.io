45424e78a3866e9c472b327fd8288798fdb431d3
consolexinhun <consolexin@gmail.com>
2021-11-21 22:38:19 +0800

    多重背包

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -13,6 +13,9 @@ module.exports = [
   {
     text: "C++", link: "/cplusplus/", items:[
       {text: "c++基础", link: "/pages/aa80a1/"},
+      {text: "相关书籍", items:[
+          {text: "c++与数据结构", link: "cplusplus_and_data_struct/"}
+      ]}
     ]
   },
   {
@@ -25,20 +28,3 @@ module.exports = [
     text: '归档', link: '/archives/'
   }
 ]
-
-  // {
-  //   text: '前端', link: '/web/', items: [
-  //     // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-  //     {
-  //       text: '前端文章', items: [
-  //         { text: 'JavaScript', link: '/pages/0f6a0ac99b62ede5/' },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: '页面', link: '/ui/', items: [
-  //     { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
-  //     { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
-  //   ],
-  // },
