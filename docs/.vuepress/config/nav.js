832e41c341cd7dfcccbc129d2a7a2f65c1fe13b8
consolexinhun <consolexin@gmail.com>
2021-11-21 23:11:10 +0800

    多重背包

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -3,11 +3,14 @@ module.exports = [
   { text: '首页', link: '/' },
   {
     text: "算法", link: "/algorithm/", items:[
-      {text: "图论", link: "/pages/5a514b/"},
-      {text: "字符串", link: "/pages/3ce601/"},
-      {text: "动态规划", link: "/pages/37ebac/"},
-      {text: "二分", link: "/pages/cc5f84/"},
-      {text: "滑动窗口", link: "/pages/30b0a0/"},
+      {text: "算法基础", items:[
+        {text: "图论", link: "/pages/5a514b/"},
+        {text: "字符串", link: "/pages/3ce601/"},
+        {text: "动态规划", link: "/pages/37ebac/"},
+        {text: "二分", link: "/pages/cc5f84/"},
+        {text: "滑动窗口", link: "/pages/30b0a0/"},
+      ]},
+      
     ],
   },
   {
