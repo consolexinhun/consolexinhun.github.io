c88720b809bf0cb5b91df384f0da2fd4a3962536
dengyang <consolexin@gmail.com>
2024-09-12 01:54:59 -0700

    doc

--- scripts/gen_num.py
+++ scripts/gen_num.py
@@ -1,11 +1,20 @@
 import os
 import sys
+import re
+
+PATTERN = r"^\d{1,3}\."
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "docs"))
 
 if __name__ == '__main__':
-    dirpath = sys.argv[1]
-    files = sorted(os.listdir(dirpath))
-    for i, file in enumerate(files):
-        new_name = "{:03d}.{}".format(i, file)
-        os.rename(os.path.join(dirpath, file), os.path.join(dirpath, new_name))
+    for root, dirs, files in os.walk(ROOT):
+        for dirpath in dirs:
+            if dirpath.startswith('.') or dirpath.startswith('@') or root.__contains__('.vuepress'):
+                continue
+            files = sorted(os.listdir(os.path.join(root, dirpath)))
+            for i, file in enumerate(files):
+                if not re.match(PATTERN, file):
+                    new_name = "{:03d}.{}".format(i, file)
+                    os.rename(os.path.join(root, dirpath, file), os.path.join(root, dirpath, new_name))
 
 
