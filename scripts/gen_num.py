9495ab80a38ef427904f63924c17861edd693078
dengyang <consolexin@gmail.com>
2024-09-12 01:13:15 -0700

    rename

---
+++ scripts/gen_num.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+if __name__ == '__main__':
+    dirpath = sys.argv[1]
+    files = sorted(os.listdir(dirpath))
+    for i, file in enumerate(files):
+        new_name = "{:03d}{}".format(i, file)
+        os.rename(os.path.join(dirpath, file), os.path.join(dirpath, new_name))
+
+
