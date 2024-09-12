4558867019c3a423689554e064f3651027eace2a
dengyang <consolexin@gmail.com>
2024-09-12 01:13:56 -0700

    rename

--- scripts/gen_num.py
+++ scripts/gen_num.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     dirpath = sys.argv[1]
     files = sorted(os.listdir(dirpath))
     for i, file in enumerate(files):
-        new_name = "{:03d}{}".format(i, file)
+        new_name = "{:03d}.{}".format(i, file)
         os.rename(os.path.join(dirpath, file), os.path.join(dirpath, new_name))
 
 
